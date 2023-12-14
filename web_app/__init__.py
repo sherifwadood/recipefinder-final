08d612310e96e3adfae6d9ef201542c4a5a630dd
sherifwadood <sma337@georgetown.edu>
2023-12-14 11:45:38 -0500

    Update __init__.py

---
+++ web_app/__init__.py
@@ -0,0 +1,25 @@
+# this is the "web_app/__init__.py" file...
+import os
+
+from flask import Flask
+
+from web_app.routes.home_routes import home_routes
+from web_app.routes.featured_recipes_routes import featured_recipes_routes
+
+
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.register_blueprint(home_routes)
+    app.register_blueprint(featured_recipes_routes)
+
+
+
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
